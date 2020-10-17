30a4acfc4066e5be72c70867d98d23c141202c08
yaoygTony <569653256@qq.com>
2020-10-17 18:31:50 -0400

    no data files in git

--- TRADES-master/trades.py
+++ TRADES-master/trades.py
@@ -29,7 +29,7 @@ def trades_loss(model,
     model.eval()
     batch_size = len(x_natural)
     # generate adversarial example
-    x_adv = x_natural.detach() + 0.001 * torch.randn(x_natural.shape).cuda().detach()
+    x_adv = x_natural.detach() + 0.001 * torch.randn(x_natural.shape).cpu().detach()
     if distance == 'l_inf':
         for _ in range(perturb_steps):
             x_adv.requires_grad_()
@@ -41,7 +41,7 @@ def trades_loss(model,
             x_adv = torch.min(torch.max(x_adv, x_natural - epsilon), x_natural + epsilon)
             x_adv = torch.clamp(x_adv, 0.0, 1.0)
     elif distance == 'l_2':
-        delta = 0.001 * torch.randn(x_natural.shape).cuda().detach()
+        delta = 0.001 * torch.randn(x_natural.shape).cpu().detach()
         delta = Variable(delta.data, requires_grad=True)
 
         # Setup optimizers
